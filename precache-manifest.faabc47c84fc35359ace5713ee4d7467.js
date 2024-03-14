ee8614810dfff1d941f4960e63abd4cbb4276bfb
yc896 <yash16chaudhary@gmail.com>
2024-03-13 22:24:29 -0400

    Updates

---
+++ precache-manifest.faabc47c84fc35359ace5713ee4d7467.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f244147d3b4c00884edb373b0eb31b4a",
+    "url": "/react-playlist/index.html"
+  },
+  {
+    "revision": "1beb52bfedd10a66c36b",
+    "url": "/react-playlist/static/css/main.8f530957.chunk.css"
+  },
+  {
+    "revision": "8822e8a4086e073aa836",
+    "url": "/react-playlist/static/js/2.7bd8efa2.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/react-playlist/static/js/2.7bd8efa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32a0e8047f5d0ae39f0d",
+    "url": "/react-playlist/static/js/3.7fa99968.chunk.js"
+  },
+  {
+    "revision": "1beb52bfedd10a66c36b",
+    "url": "/react-playlist/static/js/main.60795281.chunk.js"
+  },
+  {
+    "revision": "60b473fff4afcf293205",
+    "url": "/react-playlist/static/js/runtime-main.377d816e.js"
+  },
+  {
+    "revision": "152cd78f80d7018454b2fc82a0fc9795",
+    "url": "/react-playlist/static/media/GreenGuitarLogo.152cd78f.png"
+  }
+]);
